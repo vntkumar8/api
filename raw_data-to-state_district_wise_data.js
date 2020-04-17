68114fa206336ec16f82c70a8eb2b4b469a39e7d
nishcthulhu <nishthedevil@gmail.com>
2020-04-17 20:47:50 +0530

    Correcting use of moment function to persist district deltas till 7AM the next day (#128)
    * Allow district delta time till 730 AM IST to be in sync with state delta
    
    * Change moment function usage to accurately capture district delta, in line with state delta
    
    * Corrected moment function use to persist district deltas upto 7AM the next day
    
    Co-authored-by: Jun <junaidvayoli@gmail.com>

--- raw_data-to-state_district_wise_data.js
+++ raw_data-to-state_district_wise_data.js
@@ -5,7 +5,8 @@ const rawData = require('./raw_data');
 console.log('Starting district wise data processing');
 try {
   const StateDistrictWiseData = rawData.raw_data.reduce((acc, row) => {
-    const isToday = moment().utcOffset(420).isSame(moment(row.dateannounced, "DD-MM-YYYY"), "day");
+    const todaysdate = moment().utcOffset(330); //set moment instance to IST timezone and fetch currect IST time
+    const isToday = moment(todaysdate).add(-420, "m").isSame(moment(row.dateannounced + " 00:00 +0530", "DD-MM-YYYY HH:mm Z"), "day"); //Subtract 7 hours from IST time to persist values upto 7 AM and create moment instance from row.dateannounced in the IST Timezone
     let stateName = row.detectedstate;
       if(!stateName) {
         return acc;
