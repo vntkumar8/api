d1e6bc41991c3da958e473c893fa136f4cac62d4
Jun <junaidvayoli@gmail.com>
2020-04-22 13:13:34 +0900

    merge csv

--- src/sheets-to-csv.js
+++ src/sheets-to-csv.js
@@ -42,51 +42,73 @@ const all_sheets = [
 const old_sheets = [
     ["raw_data1", "0"],
     ["death_and_recovered1", "200733542"],
-    // ["state_wise", "1896310216"],
-    // ["state_wise_daily", "1395461826"],
-    // ["sources_list", "704389477"],
-    // ["statewise_tested_numbers_data", "486127050"],
-    // ["case_time_series", "387368559"],
-    // ["tested_numbers_icmr_data", "2143634168"],
-    // ["travel_history", "1532084277"]
 ];
-old_sheets.forEach(element => {
-    console.log("Reading: " + element[0]);
-    var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_1 + "/pub?gid=" + element[1] + "&single=false&output=csv";
-    console.log(temp_url);
-    url = encodeURI(temp_url);
-    let settings = { method: "Get" };
-    fetch(url, settings).then(res => res.text())
-        .then(csv => {
-            if (csv.includes("</html>")) {
-                console.error("probably not csv!");
-                process.exit(1);
-                return;
-            } else {
-                fs.writeFileSync(today_dir + "/" + element[0] + ".csv", csv);
-                fs.writeFileSync(latest_dir + "/" + element[0] + ".csv", csv);
-                console.log("Write completed: " + element[0]);
-            }
-        });
-});
 
 
-all_sheets.forEach(element => {
-    console.log("Reading: " + element[0]);
-    var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_2 + "/pub?gid=" + element[1] + "&single=false&output=csv";
-    console.log(temp_url);
-    url = encodeURI(temp_url);
-    let settings = { method: "Get" };
-    fetch(url, settings).then(res => res.text())
-        .then(csv => {
-            if (csv.includes("</html>")) {
-                console.error("probably not csv!");
-                process.exit(1);
-                return;
-            } else {
-                fs.writeFileSync(today_dir + "/" + element[0] + ".csv", csv);
-                fs.writeFileSync(latest_dir + "/" + element[0] + ".csv", csv);
-                console.log("Write completed: " + element[0]);
-            }
-        });
-});
+(async function main() {
+    // uncomment below and run when changes in v1 sheet
+    // for (var element of old_sheets) {
+    //     console.log("Reading: " + element[0]);
+    //     var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_1 + "/pub?gid=" + element[1] + "&single=false&output=csv";
+    //     console.log(temp_url);
+    //     url = encodeURI(temp_url);
+    //     let settings = { method: "Get" };
+    //     await fetch(url, settings).then(res => res.text())
+    //         .then(csv => {
+    //             if (csv.includes("</html>")) {
+    //                 console.error("probably not csv!");
+    //                 process.exit(1);
+    //                 return;
+    //             } else {
+    //                 fs.writeFileSync(today_dir + "/" + element[0] + ".csv", csv);
+    //                 fs.writeFileSync(latest_dir + "/" + element[0] + ".csv", csv);
+    //                 console.log("Write completed: " + element[0]);
+    //             }
+    //         });
+    // };
+
+    for (var element of all_sheets) {
+        console.log("Reading: " + element[0]);
+        var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_2 + "/pub?gid=" + element[1] + "&single=false&output=csv";
+        console.log(temp_url);
+        url = encodeURI(temp_url);
+        let settings = { method: "Get" };
+        await fetch(url, settings).then(res => res.text())
+            .then(csv => {
+                if (csv.includes("</html>")) {
+                    console.error("probably not csv!");
+                    process.exit(1);
+                    return;
+                } else {
+                    fs.writeFileSync(today_dir + "/" + element[0] + ".csv", csv);
+                    fs.writeFileSync(latest_dir + "/" + element[0] + ".csv", csv);
+                    console.log("Write completed: " + element[0]);
+                }
+            });
+    }
+
+    // concat steps below
+    console.log("merge both csv");
+    var raw_data1 = fs.readFileSync('./tmp/csv/latest/raw_data1.csv', 'utf8');
+    var raw_data2 = fs.readFileSync('./tmp/csv/latest/raw_data2.csv', 'utf8');
+    var deaths_recoveries1 = fs.readFileSync('./tmp/csv/latest/death_and_recovered1.csv', 'utf8');
+    var deaths_recoveries2 = fs.readFileSync('./tmp/csv/latest/death_and_recovered2.csv', 'utf8');
+
+    dr = deaths_recoveries2.split("\n");;
+    dr.shift();
+    dr2 = dr.join("\n");
+    deaths_recoveries = deaths_recoveries1 + "\n" + dr2;
+
+    rd = raw_data2.split("\n");;
+    rd.shift();
+    rd2 = rd.join("\n");
+    raw_data = raw_data1 + "\n" + rd2;
+
+    fs.writeFileSync(today_dir + "/raw_data.csv", raw_data);
+    fs.writeFileSync(latest_dir + "/raw_data.csv", raw_data);
+    console.log("merged raw_data1 and raw_data2");
+    fs.writeFileSync(today_dir + "/death_and_recovered.csv", deaths_recoveries);
+    fs.writeFileSync(latest_dir + "/death_and_recovered.csv", deaths_recoveries);
+    console.log("merged death_and_recovered1 and death_and_recovered2");
+
+})();
