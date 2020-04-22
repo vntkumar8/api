4a67c1e9a2d9c6160f349b9e5567d8979bf856f0
Jun <junaidvayoli@gmail.com>
2020-04-22 12:11:15 +0900

    sheet to csv for both old and new

--- src/sheets-to-csv.js
+++ src/sheets-to-csv.js
@@ -24,11 +24,12 @@ if (!fs.existsSync(latest_dir)) {
     fs.mkdirSync(latest_dir);
 }
 
-const PUBLISHED_SHEET_ID = "2PACX-1vSz8Qs1gE_IYpzlkFkCXGcL_BqR8hZieWVi-rphN1gfrO3H4lDtVZs4kd0C3P8Y9lhsT1rhoB-Q_cP4";
+const PUBLISHED_SHEET_ID_1 = "2PACX-1vSz8Qs1gE_IYpzlkFkCXGcL_BqR8hZieWVi-rphN1gfrO3H4lDtVZs4kd0C3P8Y9lhsT1rhoB-Q_cP4";
+const PUBLISHED_SHEET_ID_2 = "2PACX-1vRodtoTyQwXckfuvuQllkMhGC_gruigaaizVc8I6-BZWeetYpmRyexnO75ep7rnSxFICd8c9dfpwU8I";
 
 const all_sheets = [
-    ["raw_data", "0"],
-    ["death_and_recovered", "200733542"],
+    ["raw_data2", "0"],
+    ["death_and_recovered2", "200733542"],
     ["state_wise", "1896310216"],
     ["state_wise_daily", "1395461826"],
     ["sources_list", "704389477"],
@@ -38,9 +39,41 @@ const all_sheets = [
     ["travel_history", "1532084277"]
 ];
 
+const old_sheets = [
+    ["raw_data1", "0"],
+    ["death_and_recovered1", "200733542"],
+    // ["state_wise", "1896310216"],
+    // ["state_wise_daily", "1395461826"],
+    // ["sources_list", "704389477"],
+    // ["statewise_tested_numbers_data", "486127050"],
+    // ["case_time_series", "387368559"],
+    // ["tested_numbers_icmr_data", "2143634168"],
+    // ["travel_history", "1532084277"]
+];
+old_sheets.forEach(element => {
+    console.log("Reading: " + element[0]);
+    var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_1 + "/pub?gid=" + element[1] + "&single=false&output=csv";
+    console.log(temp_url);
+    url = encodeURI(temp_url);
+    let settings = { method: "Get" };
+    fetch(url, settings).then(res => res.text())
+        .then(csv => {
+            if (csv.includes("</html>")) {
+                console.error("probably not csv!");
+                process.exit(1);
+                return;
+            } else {
+                fs.writeFileSync(today_dir + "/" + element[0] + ".csv", csv);
+                fs.writeFileSync(latest_dir + "/" + element[0] + ".csv", csv);
+                console.log("Write completed: " + element[0]);
+            }
+        });
+});
+
+
 all_sheets.forEach(element => {
     console.log("Reading: " + element[0]);
-    var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID + "/pub?gid=" + element[1] + "&single=false&output=csv";
+    var temp_url = "https://docs.google.com/spreadsheets/d/e/" + PUBLISHED_SHEET_ID_2 + "/pub?gid=" + element[1] + "&single=false&output=csv";
     console.log(temp_url);
     url = encodeURI(temp_url);
     let settings = { method: "Get" };
